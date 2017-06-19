7aa13911a545850a6cd98704d30fd1f6ecb39354
Vivek Bhadra <vivek.bhadra@gmail.com>
2017-06-19 22:12:46 +0100

    Fix merge conflict.
    Signed-off-by: Vivek Bhadra <vivek.bhadra@gmail.com>

--- multi-threaded/common.h
+++ multi-threaded/common.h
@@ -2,14 +2,6 @@
 #define COMMON_H
 #endif
 
-<<<<<<< HEAD
-=======
-#include <pthread.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <error.h>
-
->>>>>>> ed975e7d44293936fb248b91d6b7672db7ebe28a
 #define MESSAGE_LEN 2000
 
 typedef enum request {
@@ -22,15 +14,3 @@ typedef struct message {
     char message[MESSAGE_LEN];
 }message_t;
 
-<<<<<<< HEAD
-typedef struct server_data {
-    char message[BUF_LEN];
-    pthread_mutex_t rwsem;
-    FILE *fp;
-}server_data_t;
-
-extern int initialize_database(server_data_t *servData, const char *db_name);
-extern char * read_database(server_data_t *servData, const char *db_name);
-extern void write_database(void);
-=======
->>>>>>> ed975e7d44293936fb248b91d6b7672db7ebe28a
